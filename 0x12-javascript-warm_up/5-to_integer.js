40fad5c6a8b5029e40edcc05ae4ffb570764b97d
soroel <sorophinenoel@gmail.com>
2022-09-07 18:06:47 +0000

    corrections

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-'use strict';
-let arg = process.argv[2];
-if (isNaN(arg)) {
-  console.log('Not a number');
+const num = process.argv[2];
+if (!isNaN(num)) {
+  console.log('My number: ' + parseInt(num));
 } else {
-  console.log('My number: ' + arg);
+  console.log('Not a number');
 }
