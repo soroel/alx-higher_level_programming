c05007844f5e970d8d6d347a7a04aff82750e63a
soroel <sorophinenoel@gmail.com>
2022-09-07 18:09:27 +0000

    corrections

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-'use strict';
-let x = process.argv[2];
-if (isNaN(x)) {
+const size = process.argv[2];
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+  for (let i = 0; i < parseInt(size); i++) {
+    console.log('X'.repeat(size));
   }
 }
