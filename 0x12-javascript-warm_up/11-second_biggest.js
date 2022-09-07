07922d24fee93eb5206182a865c66e79567fb591
soroel <sorophinenoel@gmail.com>
2022-09-07 18:12:56 +0000

    corrections

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
-'use strict';
-let nextMax = 0;
-let args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  nextMax = args[args.length - 2];
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  let big = 0;
+  let sec = 0;
+
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg_ = parseInt(process.argv[i]);
+    if (arg_ > big) {
+      big = arg_;
+    }
+  }
+
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg_ = parseInt(process.argv[i]);
+    if (arg_ > sec && arg_ < big) sec = arg_;
+  }
+
+  console.log(sec);
 }
-console.log(nextMax);
