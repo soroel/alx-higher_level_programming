179d6a1c8ea780514f325ca859bcde099cc720b5
soroel <sorophinenoel@gmail.com>
2022-09-07 18:10:18 +0000

    corrections

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-'use strict';
-let a = process.argv[2];
-let b = process.argv[3];
+const first = parseInt(process.argv[2]);
+const sec = parseInt(process.argv[3]);
+
 function add (a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return (NaN);
-  } else {
-    return (parseInt(a) + parseInt(b));
-  }
+  const sum = a + b;
+  console.log(sum);
 }
-console.log(add(a, b));
+add(first, sec);
